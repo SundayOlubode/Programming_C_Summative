3b4c62bfe24c3113f2a4d4749a1947f82dbee103
Sam <s.olubode@alustudent.com>
2024-07-28 07:25:52 +0100

    update(init): rename files

---
+++ q1_sorting_names.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - sort names in asc | desc order
+ *
+ * Task 1: You are having a function pointer that will be taking the array \
+ * of names and a callback function that will be used to sort names \
+ * in the chosen order.
+ * Task 2: You are having a second function will count the number of \
+ * names in an array.
+ * Task 3: You will need to have an array of functions such that \
+ * the user chooses 1 for function 1 and 2 for function 2.
+ *
+ * Return: {0} - success | {1} - failed
+ */
+void main()
+{
+	printf("Sort names\n");
+}
